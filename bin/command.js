5b3b2ccfc95d6cf080a0b3d113130f90bc5d9208
KuangPF <1633397286@qq.com>
2018-10-23 23:22:41 +0800

    add init command

--- bin/command.js
+++ bin/command.js
@@ -2,12 +2,13 @@
 
 const program = require('commander')
 
+const init = require('../lib/init') // init
+
 program
   .version(require('../package.json').version, '-v, --version')
 
 program
   .command('init')
-  .description('init your cli')
-
-
+  .description('generate a new project from a template')
+  .action(init)
 program.parse(process.argv)
