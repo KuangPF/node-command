1b1d3d6122cedf93a2cd0c4a03881c05ca0fe673
KuangPF <1633397286@qq.com>
2018-10-28 18:30:56 +0800

    feat: view version with -V

--- bin/iview.js
+++ bin/iview.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-
 const init = require('../lib/init') // init
 
 program
-  .version(require('../package.json').version, '-v, --version')
+  .version(require('../package.json').version, '-V, --version')
+  .usage('<command> [options]')
 
 program
   .command('init <project-name>')
+  .description('create a new project powered by iview-cli')
   .action(function (projectName) {
-    init(projectName)
+    program.outputHelp()
+    // init(projectName)
   })
 
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+  })
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
 program.parse(process.argv)
